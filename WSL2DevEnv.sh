7fb7eec07cf1b6ef4d9f47739689d94f52c81bf3
Albaro Pereyra <AlbaroPereyra22@gmail.com>
2024-04-17 02:08:58 -0700

    default settins

--- WSL2DevEnv.sh
+++ WSL2DevEnv.sh
@@ -1,3 +1,12 @@
 #! /bin/sh -
 sudo snap install emacs --classic
 sudo apt-get install zsh
+git clone git@github.com:AlbaroPereyra/zshconf.git
+chmod u+x zshconf/install.sh
+cd zshconf
+./install.sh
+git clone git@github.com:AlbaroPereyra/emacsconf.git
+chmod u+x emacsconf/installer.sh
+cd emacsconf
+./installer.sh
+sudo apt-get install emacs
