bac054c0521e82c7db890d95fa5706432ae76f27
Albaro Pereyra <AlbaroPereyra22@gmail.com>
2024-06-14 14:37:34 -0700

    Added WSL2DevEnv back to repo

---
+++ WSL2DevEnv.sh
@@ -0,0 +1,18 @@
+#! /bin/sh -
+
+# set local
+sudo locale-gen "en_US.UTF-8"
+#sudo dpkg-reconfigure locales
+sudo snap install emacs --classic
+sudo apt-get install zsh net-tools
+git clone git@github.com:AlbaroPereyra/zshconf.git
+chmod u+x zshconf/install.sh
+cd zshconf
+./install.sh
+git clone git@github.com:AlbaroPereyra/emacsconf.git
+chmod u+x emacsconf/installer.sh
+cd emacsconf
+./installer.sh
+sudo apt-get install emacs
+git clone git@github.com:AlbaroPereyra/screenconf.git
+mv screenconf/.screenrc ~
