d1d155132203bd9e318386a574a49cadc0912281
Albaro Pereyra <AlbaroPereyra22@gmail.com>
2024-06-19 20:39:46 -0700

    Added brotil

--- WSL2drogonInstall.sh
+++ WSL2drogonInstall.sh
@@ -1,24 +1,30 @@
 #! /bin/sh -
 
 sudo apt-get update;
-sudo apt install git gcc g++ cmake uuid-dev zlib1g-dev openssl libssl-dev postgresql-all libmariadb-dev libsqlite3-dev libhiredis-dev mariadb-server doxygen brotli;
-sudo mysql_secure_installation;
+sudo apt install git gcc g++ cmake uuid-dev zlib1g-dev openssl libssl-dev postgresql-all libmariadb-dev libsqlite3-dev libhiredis-dev mariadb-server doxygen curl zip unzip tar pkg-config;
+#sudo mysql_secure_installation;
+cd;
 git clone https://github.com/open-source-parsers/jsoncpp;
 cd jsoncpp/;
 mkdir build
 cd build
 cmake ..
 sudo make && sudo make install
-cd ../..
+cd;
+git clone https://github.com/Microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg integrate install
+./vcpkg install brotli
+cd;
 git clone https://github.com/drogonframework/drogon
 cd drogon
 git submodule update --init
 mkdir build
 cd build
-cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake ..
 make && sudo make install
 # To use SQLite.
 # debug
 sudo echo "appendWindowsPath=false" >> /etc/wsl.conf
 dg_ctl -v;
-
--- WSL2uname.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh -
-echo $(uname -a)
-uname=$(uname -a)
-tmpWSL2=${uname##*-}
-WSL2=${tmpWSL2%% *}
-echo $WSL2
-
