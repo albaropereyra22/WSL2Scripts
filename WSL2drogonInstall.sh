9e9aa61bccd4cf86029df1dee6f1f9b24a6cbe9c
Albaro Pereyra <AlbaroPereyra22@gmail.com>
2024-06-23 04:42:55 -0700

    Added Yaml

--- WSL2drogonInstall.sh
+++ WSL2drogonInstall.sh
@@ -1,9 +1,10 @@
 #! /bin/sh -
 
 sudo apt-get update;
-sudo apt install git gcc g++ cmake uuid-dev zlib1g-dev openssl libssl-dev postgresql-all libmariadb-dev libsqlite3-dev libhiredis-dev mariadb-server doxygen libbrotli-dev;
+sudo apt install git gcc g++ cmake uuid-dev zlib1g-dev openssl libssl-dev postgresql-all libmariadb-dev libsqlite3-dev libhiredis-dev mariadb-server doxygen libbrotli-dev libc-ares-dev libyaml-cpp-dev;
 #sudo mysql_secure_installation;
 cd;
+rm -rf ~/jsoncpp;
 git clone https://github.com/open-source-parsers/jsoncpp;
 cd jsoncpp/;
 mkdir build
@@ -11,14 +12,14 @@ cd build
 cmake ..
 sudo make && sudo make install
 cd;
+rm -rf ~/drogon
 git clone https://github.com/drogonframework/drogon
 cd drogon
 git submodule update --init
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake ..
 make && sudo make install
-# To use SQLite.
-# debug
-sudo echo "appendWindowsPath=false" >> /etc/wsl.conf
+#TODO debug to use SQLite.
+#sudo echo "appendWindowsPath=false" >> /etc/wsl.conf
 dg_ctl -v;
